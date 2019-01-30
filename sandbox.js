4c0ee443586500e5434dd1e99ba55ef2e5324f62
Jeff Levine <jlevine_84@yahoo.com>
2019-01-30 16:32:44 -0500

    testing

---
+++ sandbox.js
@@ -0,0 +1,33 @@
+var config = {
+    apiKey: "AIzaSyBU2XossO8Lhv9HzbBdbHmQp8VkScxkvUU",
+    authDomain: "test-162e1.firebaseapp.com",
+    databaseURL: "https://test-162e1.firebaseio.com",
+    projectId: "test-162e1",
+    storageBucket: "test-162e1.appspot.com",
+    messagingSenderId: "599175453998"
+};
+firebase.initializeApp(config);
+
+// FirebaseUI config.
+var uiConfig = {
+signInSuccessUrl: 'https://jlevine84.github.io/Sandbox/test.html',
+signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+],
+// tosUrl and privacyPolicyUrl accept either url string or a callback
+// function.
+// Terms of service url/callback.
+// tosUrl: '<your-tos-url>',
+// Privacy policy url/callback.
+// privacyPolicyUrl: function() {
+//     window.location.assign('<your-privacy-policy-url>');
+// }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
