25e27ca78baac79629bd0984873085097444b979
Jeff Levine <jlevine_84@yahoo.com>
2019-02-01 15:06:02 -0500

    test

--- sandbox.js
+++ sandbox.js
@@ -25,21 +25,56 @@ $(document).ready(function() {
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
     // The start method will wait until the DOM is loaded.
     ui.start('#firebaseui-auth-container', uiConfig);
+
     
-    
+    //Checks online/offline status and displays the UI accordingly
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
            console.log("User Signed In")
             // User is signed in.
+            //Change Login UI to sign out
             var user = firebase.auth().currentUser;
-            $("#doodad").text("Welcome " + user.displayName + ". You are signed in!")
+            $("#doodad").text("Welcome " + user.displayName + ". You are signed by: " + user.PROVIDER_ID)
+            //Client's data goes here.
+
             
         } else {
             // No user is signed in.
             $("#doodad").text("Derp!")
+            //Display public data when no user is signed in or signs out.
+            
 
         }
     });
+
+    //sign up function
+    $("#sign-up").on("click", function(event) {
+        event.preventDefault();
+        var email = $("#EMSUP").val().trim();
+        var password = $("PWSUP").val().trim();
+        firebase.auth().creatUserWithEmailAndPassword(email, password)
+        .catch(function (error) {
+            alert("Signup unsuccesful. Error Code: " + error)
+        });    
+    });
+
+    //sign in function
+    $("#sign-in").on("click", function(event) {
+        event.preventDefault();
+        var email = $("#EMSIN").val().trim();
+        var password = $("#PWSIN").val().trim();
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+        .then(function () {
+            return firebase.auth().signInWithEmailAndPassword(email, password);
+        }).catch(function(error) {
+            alert("Sign-in unsuccesful. Error Code: " + error)
+        });
+    });
+
+    //signout function
+    $("#signout").on("click", function() {
+        firebase.auth().signOut();
+    })
 
 //End document ready function
 });
